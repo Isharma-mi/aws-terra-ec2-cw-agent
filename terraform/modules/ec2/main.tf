cd37a459d36d08e4b5d28789c6c394f660db0fe6
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2025-08-11 20:05:33 -0400

    feat(ec2): Added user data and Fixed SG of instance
    - Added user data to install CW Agent
    - Fixed SG of ec2 instance to allow HTTPs outbound traffic (For CW Agent installation/metric pushing)
    - TODO: Create IAM role for ec2 instance to give permissions to send data to cloudwatch

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,10 +1,16 @@
 resource "aws_instance" "web_server" {
     ami = var.ec2_ami_id
     instance_type = var.ec2_instance_type
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo yum -y install amazon-cloudwatch-agent
+    EOF
     vpc_security_group_ids = [aws_security_group.web_server_sg.id]
+    
     tags = {
         name = var.ec2_tag_name
     }
+
 }
 
 resource "aws_security_group" "web_server_sg" {
@@ -15,13 +21,22 @@ resource "aws_security_group" "web_server_sg" {
     }
 }
 
-resource "aws_vpc_security_group_egress_rule" "web_server_sg_egress" {
+resource "aws_vpc_security_group_egress_rule" "web_server_sg_egress_ssh" {
     security_group_id = aws_security_group.web_server_sg.id
     
     cidr_ipv4 = var.ec2_sg_egress_cidr_ipv4
     from_port = 22
     ip_protocol = 6
     to_port = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "web_server_sg_egress_https" {
+    security_group_id = aws_security_group.web_server_sg.id
+
+    cidr_ipv4 = var.ec2_sg_egress_cidr_ipv4
+    from_port = 443
+    ip_protocol = 6
+    to_port = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "web_server_sg_ingress" {
