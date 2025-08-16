f1c07bf6816bdaf84e4cbee411aa0c15f4b45d2f
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2025-08-16 13:56:42 -0400

    feat(iam): Created policy relating to CW permissions
    - Created a policy based off the CloudWatchAgentServerPolicy that gives permissions for uploading from the agent to CloudWatch
    - TODO: Attach the iam policy to the role with the aws_iam_policy_attachment resource
    - TODO:  Refactor name of iam_ec2_role resource to remove iam prefix (Also ensure variables have module name as prefix)
    - TODO: Create the CW namespace -> Restrict the policy to the namespace

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "iam_ec2_role" {
+resource "aws_iam_role" "iam_ec2_role" { # TODO: Refactor name so it doesn't have iam in front (Already in iam module anyways)
     name = var.iam_ec2_role_name
 
     assume_role_policy = jsonencode({
@@ -19,6 +19,34 @@ resource "aws_iam_role" "iam_ec2_role" {
     }
 }
 
-# TODO: Make the IAM policy for CW Agent
-    # CloudWatchAgentServerPolicy restricted by the namespace
-    # TODO: Restrict the policy to the namespace once CW module made
+resource "aws_iam_policy" "ec2_cw_policy" {
+    name = "CWAccessPolicy"
+    description = "Policy similar to CloudWatchAgentServerPolicy's but restricted by namespace"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "CWACloudWatchServerPermissionsModified"
+                Action = [
+                    "cloudwatch:PutMetricData",
+                    "ec2:DescribeVolumes",
+                    "ec2:DescribeTags",
+                    "logs:PutLogEvents",
+                    "logs:PutRetentionPolicy",
+                    "logs:DescribeLogStreams",
+                    "logs:DescribeLogGroups",
+                    "logs:CreateLogStream",
+                    "logs:CreateLogGroup",
+                    "xray:PutTraceSegments",
+                    "xray:PutTelemetryRecords",
+                    "xray:GetSamplingRules",
+                    "xray:GetSamplingTargets",
+                    "xray:GetSamplingStatisticSummaries"
+                ]
+                Effect = "Allow"
+                Resource = "*" # TODO: Restrict the policy to the namespace once the CW module is made
+            }
+        ]
+    })
+}
